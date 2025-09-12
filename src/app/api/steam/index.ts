0844aaedd08365a01ff6caa2ba528a08d1c15272
wuhd <100937@hlxd.com>
2025-09-12 09:21:38 +0800

    feat: :sparkles: 更新 steam api key

--- src/app/api/steam/index.ts
+++ src/app/api/steam/index.ts
@@ -1,5 +1,8 @@
 import { SteamRecentlyPlayedGame } from './types';
 
+export const STEAM_API_KEY = 'A2982B4D437D0105DC79153E49B113CB';
+export const STEAM_USER_ID = '76561198021235279';
+
 /** 获取 Steam 最近游戏 */
 export const getSteamRecentlyGames = (): Promise<SteamRecentlyPlayedGame> =>
   fetch('/api/steam/recently').then((res) => res.json());
--- src/app/api/steam/recently/route.ts
+++ src/app/api/steam/recently/route.ts
@@ -1,10 +1,11 @@
 export const runtime = 'edge';
 
 import { NextResponse } from 'next/server';
+import { STEAM_API_KEY, STEAM_USER_ID } from '../index';
 
 export async function GET() {
   const res = await fetch(
-    'http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=EFA1F72FE84908F133513654BE2CF818&steamid=76561198021235279&format=json',
+    `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${STEAM_API_KEY}&steamid=${STEAM_USER_ID}&format=json`,
   ).then((res) => res.json());
   return NextResponse.json(res.response);
 }
